55824b97b98a84e9a9167f76666f5a70ac912bf7
Andy Brown <andy@andygbrown.co.uk>
2013-05-14 17:51:50 +0100

    Users now login using Email (registration deliberately broken until next commit)
     - Updated `LoginModel` to use `Email`, not `UserName`
     - Updated the InitializeSimpleMembershipAttribute.cs to use `Email` as the Identifier (Natural Key) for users
     - Updated Views/Account/Login.cshtml to use `Email` property of `LoginModel`
     - Updated `AccountController.Login` to use `Email`, not `UserName`
     - The user "Test" can now login using their email address ("test@test.com") and their password: test123?

--- SOSamples_SimpleMembership/Models/AccountModels.cs
+++ SOSamples_SimpleMembership/Models/AccountModels.cs
@@ -59,8 +59,8 @@ namespace SOSamples_SimpleMembership.Models
     public class LoginModel
     {
         [Required]
-        [Display(Name = "User name")]
-        public string UserName { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
